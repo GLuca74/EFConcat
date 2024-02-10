a06c6eee0935b15cfc37f53b6d4bbeef69be5db2
GLuca <GLuca@DESKTOP-6Q8KE4E>
2024-02-10 09:48:29 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace EFConcat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contextOptions = new DbContextOptionsBuilder<TestContext>()
+                                                                .UseSqlServer("Data Source=DESKTOP-6Q8KE4E\\MSSQLSERVER2022;Integrated Security=SSPI;Initial Catalog=EFConcat;Encrypt=False");
+
+            using (var db = new TestContext(contextOptions.Options))
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                db.Set<City>().Select(itm=>itm.CityName.Concat("- A string")).ToArray();
+
+
+            }
+        }
+    }
+}
---
+++ TestContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFConcat
+{
+    public class TestContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        protected TestContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+        public TestContext(DbContextOptions<TestContext> options)
+        : base(options)
+        {
+        }
+        public TestContext()
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<City>().HasKey(itm => itm.CityID);
+        }
+
+    }
+
+
+
+    public class City
+    {
+        public Guid CityID { get; set; }
+        public string CityName { get; set; }
+        
+
+    }
+
+
+
+}
